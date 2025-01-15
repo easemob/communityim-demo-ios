1d7c8aa6c6c19085115fa668d3923129a1149561
lpe234 <lpe234@qq.com>
2025-01-15 22:49:49 +0800

    :sparkles: 群聊长按头像@功能 & EaseChatUiKit升级至4.12.0

---
+++ EaseChatDemo/EaseChatDemo/Main/CustomMessageListViewModel.swift
@@ -0,0 +1,21 @@
+//
+//  CustomMessageListViewModel.swift
+//  EaseChatDemo
+//
+//  Created by lpe234 on 2025/1/14.
+//
+
+import EaseChatUIKit
+
+class CustomMessageListViewModel: MessageListViewModel {
+    
+    override func messageAvatarLongPressed(profile: any ChatUserProfileProtocol) {
+        if (self.chatType == .group) {
+            if (profile.id != ChatUIKitContext.shared?.currentUserId) {
+                self.driver?.addMentionUserToField(user: profile)
+            } else {
+                consoleLogInfo("LongPress Youself Avatar", type: .debug)
+            }
+        }
+    }
+}
